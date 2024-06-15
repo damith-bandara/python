d350d0d98223cff753a589689e029e1d24f3ac17
damith-bandara <mail.dpbandara@gmail.com>
2024-06-15 12:06:44 +0530

    add commit

--- todo-app/gui.py
+++ todo-app/gui.py
@@ -4,8 +4,10 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in todo")
 input_box = sg.InputText(tooltip="Enter Todo :", key ="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values= functions.get_todos(), key = "todos", enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
-window = sg.Window('My todo app', layout=[[label], [input_box , add_button ]],font = ('Helvetica',16))
+window = sg.Window('My todo app', layout=[[label], [input_box , add_button ],[list_box , edit_button]],font = ('Helvetica',16))
 
 while True:
     event, values = window.read()
@@ -14,9 +16,17 @@ while True:
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values["todo"] + "\n"
+            new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index]= new_todo
+            
         case sg.WIN_CLOSED:
             break
 window.close()
