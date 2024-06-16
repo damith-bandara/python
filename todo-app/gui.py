9caaa9f3cc7fcf49e672c1e90317071bad1b71d9
damith-bandara <mail.dpbandara@gmail.com>
2024-06-16 15:52:19 +0530

    add edit feature

--- todo-app/gui.py
+++ todo-app/gui.py
@@ -19,14 +19,15 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
         case "Edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
-
             todos = functions.get_todos()
             index = todos.index(todo_to_edit)
             todos[index]= new_todo
-            
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
         case sg.WIN_CLOSED:
             break
 window.close()
